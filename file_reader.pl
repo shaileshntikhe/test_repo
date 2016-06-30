e8fa7e17287ab9d291bbd85e6ed753120b343486
Shailesh Tikhe <stikhe@intelli-vision.com>
2016-06-30 10:26:38 +0530

    Init

---
+++ file_reader.pl
@@ -0,0 +1,14 @@
+use strict;
+use warnings;
+
+my $input_file = {
+  if(@ARGV < 1) {
+    print("Specify input file name, exiting now");
+    exit(1)
+  }
+  else {
+    $ARGV[1]
+  }
+}
+
+print("Input file is: $input_file")
