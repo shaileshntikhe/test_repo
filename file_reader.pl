2f768ab29f03d48d6a1c0434087dcf90dde47a67
SHAILESH_TIKHE <shaileshntikhe@gmail.com>
2016-08-01 11:26:28 +0530

    Used readline method

--- file_reader.pl
+++ file_reader.pl
@@ -20,7 +20,7 @@ open(my $reader, "<", $input_file) || do {
 
 println("File: $input_file is now open for reading\n");
 
-while(my $line = <$reader>)) {
+while(my $line = readline($reader)) {
   print("Current line: $line");
 }
 
