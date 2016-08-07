1e8026d9eb579d92e54e654d9b82656c451020ce
SHAILESH_TIKHE <shaileshntikhe@gmail.com>
2016-08-07 14:31:31 +0530

    Added semi colons

--- file_reader.pl
+++ file_reader.pl
@@ -5,11 +5,12 @@ use diagnostics;
 my $input_file = {
   if(@ARGV < 1) {
     print("Specify input file name, exiting now");
-    exit(1)
+    exit(1);
   }
   else {
-    $ARGV[1]
+    $ARGV[1];
   }
 }
 
-print("Input file is: $input_file")
+print("Input file is: $input_file");
+print("Completed program");
